6d93504c1e506055c91851f7cb82ed09ca310b6f
Oscar-Cederberg-Johanneberg <oscar.cederberg@elev.ga.ntig.se>
2023-01-31 11:31:40 +0100

    review
    keadwa

--- app.rb
+++ app.rb
@@ -6,11 +6,24 @@ require 'bcrypt'
 
 get('/')  do
   slim(:start)
+end  
+
+get('/review') do
+  id = session[:id].to_i
+  db = SQLite3::Database.new('db/imdb.db')
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM RecensionId WHERE RecensionId = ?",id)
+  slim(:"/review/index",locals:{recension:result})
 end
 
-get('/reviews')
-
+get('/review/new') do
+  slim(:"review/new")
 end
 
-get('/reviews/new')
+post('/review/new') do
+  id = session[:id].to_i
+  review = params[:review]
+  db = SQLite3::Database.new('db/imdb.db')
+  db.execute("INSERT INTO recension (Content, RecensionId) VALUES(?,?)",review,id)
+  redirect('/review')
 end
